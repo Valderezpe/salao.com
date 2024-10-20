3bc5271398e6a6a3998f2b4d8fbc4144b6c0450c
valderez <valderezpe@gmail.com>
2024-10-19 22:23:18 -0300

    feat: add initial barbeshop page

--- app/_components/ui/barbershop-item.tsx
+++ app/_components/ui/barbershop-item.tsx
@@ -1,6 +1,7 @@
 import { Barbershop } from "@prisma/client"
 import { StarIcon } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 import { Badge } from "./badge"
 import { Button } from "./button"
 import { Card, CardContent } from "./card"
@@ -33,8 +34,8 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-500">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barbershop.id}`}>Reserva</Link>
           </Button>
         </div>
       </CardContent>
